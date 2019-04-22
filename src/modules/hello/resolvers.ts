9ad914058a4e6720ea29c17ed3e9fa7572301d8b
josktgui <jose.osk01@gmail.com>
2019-04-21 23:30:03 -0500

    change name

---
+++ src/modules/hello/resolvers.ts
@@ -0,0 +1,7 @@
+import { IResolvers } from 'graphql-tools';
+
+export const resolvers: IResolvers = {
+  Query: {
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || 'World'}`,
+  }
+}
